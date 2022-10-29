2f5b53ab459a6cab8aa73936867b97932c1ec104
yamaguchi03 <108157117+haru-ymth@users.noreply.github.com>
2022-10-29 15:05:02 +0900

    Update article.js

--- article/article.js
+++ article/article.js
@@ -9,7 +9,7 @@ var com2;var com3;
     var commenth2=document.createElement("h2");
     commenth2.innerHTML="コメント";
     var main=document.createElement("main");
-    main.innerHTML=`<a href="https://scratch.mit.edu/projects/749865423/">ここ</a>に<a id="comkey" style="cursor:pointer" title="クリックでコピー">${key}</a>を先頭につけてコメントしてください。また、<a id="all" style="cursor:pointer" title="クリックでコピー">[all]</a>を使うことですべての記事にコメントできます。なお、返信にはこれらのものは付ける必要はありません。<br>Scratchのアカウントが必要です。反映まで数時間かかります。ちなみにまだ試作段階なのでうまくいくかわかりません。`;
+    main.innerHTML=`<a href="https://scratch.mit.edu/projects/749865423/">ここ</a>に<a id="comkey" style="cursor:pointer" title="クリックでコピー">${key}</a>を先頭につけてコメントしてください。また、<a id="all" style="cursor:pointer" title="クリックでコピー">[all]</a>を使うことですべての記事にコメントできます。なお、返信にはこれらのものは付ける必要はありません。<br>Scratchのアカウントが必要です。反映まで数時間かかります。<br><a style="opacity:0.8">このコメント機能は<a href="https://yukkku.github.io/">yukkuさんのホームページ</a>を参考にyamaguchi03がオリジナルで作ったものです。</a>`;
     document.body.appendChild(commenth2);
     document.body.appendChild(main);
 com1.innerHTML="<button>コメントを読み込む</button>";
