ff77db1456fa5407551d72560b2dc4ff0a8443c5
yamaguchi03 <108157117+haru-ymth@users.noreply.github.com>
2022-11-05 18:25:54 +0900

    Update include.js

--- include.js
+++ include.js
@@ -11,18 +11,32 @@ window.addEventListener('DOMContentLoaded', function() {
   header.innerHTML=`
   
   <!--ここから-->
-<div class="n">
+<div class="n" style="position:relative;position:fixed">
 <a href="https://1998ky262.github.io/Scratch-intro-com/" class="white">ホーム</a>
+<a class="white" style="display:inline-block;position:absolute;right:10px;font-size:20px;cursor:pointer" id="sidebartoggle">News</a>
 </div>
+
 <!--ここまでヘッダーの内容-->
-  `
+  `;
   var ele =document.body.firstChild;
   ele.before(header);
 	if(location.href.includes("article")){
 	var article=document.createElement("script");
 	article.src="https://1998ky262.github.io/Scratch-intro-com/article/article.js";
 	document.body.appendChild(article);
-	}
+  }
+  var newsbox=document.createElement("iframe");
+  newsbox.src="https://1998ky262.github.io/Scratch-intro-com/news.html";
+  newsbox.id="newsbox";
+  document.body.before(newsbox);
+  let newsbutton=document.getElementById("sidebartoggle");
+  newsbutton.addEventListener("click",function(){
+  	if(newsbox.style.visibility=="hidden"){
+    	newsbox.style.visibility="visible";
+    }else{
+    	newsbox.style.visibility="hidden";
+    }
+  });
+
 
 });
-
